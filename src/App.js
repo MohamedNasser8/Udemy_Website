89c92cc4f05cc7820818c2e0a778fc0f91195356
Mohamed Nasser <nassermohamed3222@gmail.com>
2022-08-31 01:37:53 +0200

    Added Course Section

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+//import Greet from "./component/Greet";
+//import Welcome from "./component/Welcome";
+import CourseContainer from "./component/CourseContainer";
+function App() {
+  return (
+    <div className="App">
+      <CourseContainer></CourseContainer>
+    </div>
+  );
+}
+
+export default App;
