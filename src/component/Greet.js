21067d0e1c12bc655fe555e3745c2990c5dc68da
Mohamed Nasser <nassermohamed3222@gmail.com>
2022-08-31 01:12:26 +0200

    main branch

--- src/component/Greet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// function Greet() {
-//   return <h1>Hello Mohamad</h1>;
-// }
-
-const Greet = () => <h1>Hello Mido</h1>;
-export default Greet;
--- src/component/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class Welcome extends Component {
-  render() {
-    return <h1>Welcom Mido</h1>;
-  }
-}
-export default Welcome;
